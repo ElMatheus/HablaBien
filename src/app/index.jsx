cc50157970e7f8fff0730526aa227b6980145aba
ElMatheus <matheus.goncalves5@aluno.senai.br>
2023-12-19 15:02:58 -0300

    fit: fonts and linking

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,12 +1,43 @@
-import { View, Text } from "react-native"
+import { StyleSheet, View, Text } from "react-native";
+import { Link } from "expo-router";
+import { useFonts } from 'expo-font';
+
 
 
 const Home = () => {
+  const [fontsLoaded] = useFonts({
+    'Poppins': require('../../fonts/Poppins-LightItalic.ttf'),
+  });
   return (
     <View>
+      <View style={styles.header}>
+        <Text style={styles.titleHeader}>HablaBien</Text>
+      </View>
+
+
+
+
       <Text>dsdsd</Text>
+      <Link href="/ListenAndRepeat/page">teste</Link>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  header: {
+    backgroundColor: '#000',
+    color: '#fff',
+    padding: 12,
+    
+    
+  },
+  titleHeader: {
+    color: '#fff',
+    fontSize: 20,
+    fontFamily: 'Poppins',
+    fontStyle: 'italic',
+    fontWeight: 'bold',
+  }
+});
 
 export default Home
