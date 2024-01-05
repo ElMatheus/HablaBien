847c2604d3f3ae6886c699a8609ee0d299da05b9
ElMatheus <matheus.goncalves5@aluno.senai.br>
2024-01-05 17:24:35 -0300

    feath: design listen and repeat

--- src/app/ListenAndRepeat/page.jsx
+++ src/app/ListenAndRepeat/page.jsx
@@ -1,6 +1,31 @@
-import { View, Image, Button } from "react-native";
+import { View, Image, Button, StyleSheet, Text } from "react-native";
 import { useState, useEffect } from "react";
 import allObjects from "../../data/exercises/objects.js";
+import AppLoading from 'expo-app-loading';
+import { TouchableOpacity } from "react-native-gesture-handler";
+import Icon from 'react-native-vector-icons/AntDesign';
+import IconE from 'react-native-vector-icons/Entypo';
+import {
+    useFonts,
+    Poppins_100Thin,
+    Poppins_100Thin_Italic,
+    Poppins_200ExtraLight,
+    Poppins_200ExtraLight_Italic,
+    Poppins_300Light,
+    Poppins_300Light_Italic,
+    Poppins_400Regular,
+    Poppins_400Regular_Italic,
+    Poppins_500Medium,
+    Poppins_500Medium_Italic,
+    Poppins_600SemiBold,
+    Poppins_600SemiBold_Italic,
+    Poppins_700Bold,
+    Poppins_700Bold_Italic,
+    Poppins_800ExtraBold,
+    Poppins_800ExtraBold_Italic,
+    Poppins_900Black,
+    Poppins_900Black_Italic,
+} from '@expo-google-fonts/poppins';
 
 const ListenAndRepeat = () => {
     const [unseenObjects, setUnseenObjects] = useState([...allObjects]);
@@ -19,12 +44,83 @@ const ListenAndRepeat = () => {
         setNumberPage(Math.floor(Math.random() * unseenObjects.length));
     }, [unseenObjects]);
 
-    return (
-        <View>
-            <Image source={{ uri: unseenObjects[numberPage]?.image }} style={{ width: 100, height: 100 }} />
-            <Button title="Next" onPress={nextPage} />
-        </View>
-    )
-}
+    let [fontsLoaded] = useFonts({
+        Poppins_100Thin,
+        Poppins_100Thin_Italic,
+        Poppins_200ExtraLight,
+        Poppins_200ExtraLight_Italic,
+        Poppins_300Light,
+        Poppins_300Light_Italic,
+        Poppins_400Regular,
+        Poppins_400Regular_Italic,
+        Poppins_500Medium,
+        Poppins_500Medium_Italic,
+        Poppins_600SemiBold,
+        Poppins_600SemiBold_Italic,
+        Poppins_700Bold,
+        Poppins_700Bold_Italic,
+        Poppins_800ExtraBold,
+        Poppins_800ExtraBold_Italic,
+        Poppins_900Black,
+        Poppins_900Black_Italic,
+    });
+    if (!fontsLoaded) {
+        return <AppLoading />;
+    } else {
+        return (
+            <View>
+                <View>
+                    <Image style={styles.imgAudio} source={{ uri: unseenObjects[numberPage]?.image }} />
+                    <Text style={styles.descImg}>{unseenObjects[numberPage]?.authorCredits}</Text>
+                </View>
+                <View>
+                    <Text style={styles.title}>{unseenObjects[numberPage]?.name}</Text>
+                </View>
+                <View>
+                    <TouchableOpacity>
+                        <Icon name="setting" size={40} color="#525252"/>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <IconE name="controller-play" size={40} color="#525252" />
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <IconE name="cw" size={40} color="#525252" />
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <IconE name="level-down" size={40} color="#525252" />
+                    </TouchableOpacity>
+                </View>
+                {/* <Button title="Next" onPress={nextPage} /> */}
+            </View>
+        )
+    }
+};
+const styles = StyleSheet.create({
+    imgAudio: {
+        width: "auto",
+        aspectRatio: 1 / 1,
+        borderTopLeftRadius: 10,
+        borderTopRightRadius: 10,
+        borderBottomLeftRadius: 40,
+        borderBottomRightRadius: 40,
+        marginLeft: 5,
+        marginRight: 5,
+
+    },
+    descImg: {
+        fontFamily: 'Poppins_300Light',
+        color: '#525252',
+        textAlign: 'center',
+        marginTop: 7,
+        fontSize: 13,
+    },
+    title: {
+        fontFamily: 'Poppins_600SemiBold',
+        color: '#525252',
+        marginTop: 7,
+        fontSize: 27,
+        marginLeft: 20,
+    }
+});
 
 export default ListenAndRepeat
--- src/data/exercises/objects.js
+++ src/data/exercises/objects.js
@@ -2,7 +2,7 @@ const allObjects = [
     {
         name: 'Ancla',
         image: 'https://img.freepik.com/vetores-gratis/ancora-realista-com-corda_1284-35840.jpg',
-        authorCredits: '<Link href="https://br.freepik.com/vetores-gratis/ancora-realista-com-corda_9462235.htm#query=ancora&position=1&from_view=search&track=sph&uuid=308bc689-fc56-489c-9896-aea02fbe6abb">Imagem de macrovector</Link> no Freepik',
+        authorCredits: 'Imagem de macrovector no Freepik',
         category: 'a',
         seen: false,
         id: 1
@@ -10,7 +10,7 @@ const allObjects = [
     {
         name: 'Árbol',
         image: 'https://img.freepik.com/psd-gratuitas/cinza-isolada-em-fundo-transparente_191095-10000.jpg',
-        authorCredits: '<a href="https://br.freepik.com/psd-gratuitas/cinza-isolada-em-fundo-transparente_81702230.htm#query=three&position=0&from_view=search&track=sph&uuid=b914c537-1de1-4f32-8197-3ff864f88dbd">Imagem de tohamina</a> no Freepik',
+        authorCredits: 'Imagem de tohamina no Freepik',
         category: 'a',
         seen: false,
         id: 2
@@ -18,11 +18,111 @@ const allObjects = [
     {
         name: 'Amigo',
         image: 'https://img.freepik.com/fotos-gratis/homem-e-mulher-elegantes-posando-juntos_273609-21827.jpg',
-        authorCredits: '<a href="https://br.freepik.com/fotos-gratis/homem-e-mulher-elegantes-posando-juntos_11264925.htm#query=friend&position=0&from_view=search&track=sph&uuid=1a0152a0-1eed-4829-af64-b79fad0da355">Imagem de wayhomestudio</a> no Freepik',
+        authorCredits: 'Imagem de wayhomestudio no Freepik',
         category: 'a',
         seen: false,
         id: 3
-    }
+    },
+    {
+        name: 'Abeja',
+        image: 'https://img.freepik.com/vetores-gratis/ilustracao-de-icone-de-vetor-de-desenho-animado-bonito-abelha-voando-conceito-de-icone-de-natureza-animal-isolado-vetor-premium_138676-6016.jpg',
+        authorCredits: 'Imagem de catalyststuff no Freepik',
+        category: 'a',
+        seen: false,
+        id: 4
+    },
+    {
+        name: 'Ave',
+        image: 'https://img.freepik.com/fotos-gratis/tiro-de-foco-seletivo-de-um-beija-flor-em-voo_181624-56855.jpg',
+        authorCredits: 'Imagem de wirestock no Freepik',
+        category: 'a',
+        seen: false,
+        id: 5
+    },
+    {
+        name: 'Automóvil',
+        image: 'https://img.freepik.com/fotos-gratis/vista-do-carro-3d_23-2150796894.jpg',
+        authorCredits: 'Imagem de Freepik',
+        category: 'a',
+        seen: false,
+        id: 6
+    },
+    {
+        name: 'Avión',
+        image: 'https://img.freepik.com/psd-gratuitas/maquete-de-aviao_1310-1053.jpg',
+        authorCredits: 'Imagem de zlatko_plamenov no Freepik',
+        category: 'a',
+        seen: false,
+        id: 7
+    },
+    {
+        name: 'Bicicleta',
+        image: 'https://img.freepik.com/fotos-gratis/ciclista-masculina-andando-de-bicicleta-nas-montanhas_651396-3463.jpg',
+        authorCredits: 'Imagem de ArtPhoto_studio no Freepik',
+        category: 'a',
+        seen: false,
+        id: 8
+    },
+    {
+        name: 'Bolígrafo',
+        image: 'https://img.freepik.com/psd-gratuitas/a-ponta-da-caneta-de-feltro-isolada-sobre-fundo-transparente_191095-17718.jpg',
+        authorCredits: 'Imagem de tohamina no Freepik',
+        category: 'b',
+        seen: false,
+        id: 9
+    },
+    {
+        name: 'Bolso',
+        image: 'https://img.freepik.com/fotos-gratis/expositor-de-bolsa-feminina-levitando_23-2149817600.jpg',
+        authorCredits: 'Imagem de Freepik',
+        category: 'b',
+        seen: false,
+        id: 10
+    },
+    {
+        name: 'Bomba',
+        image: 'https://img.freepik.com/vetores-gratis/estilo-realista-de-bomba-redonda-preto_52683-15190.jpg',
+        authorCredits: 'Imagem de pikisuperstar no Freepik',
+        category: 'b',
+        seen: false,
+        id: 11
+    },
+    {
+        name: 'Botella',
+        image: 'https://img.freepik.com/psd-gratuitas/garrafa-termica-branca-simples-isolada-em-fundo-transparente_125540-5558.jpg',
+        authorCredits: 'Imagem de Vectonauta no Freepik',
+        category: 'b',
+        seen: false,
+        id: 12
+    },
+    {
+        name: 'Cama',
+        image: 'https://img.freepik.com/psd-gratuitas/cama-de-casal-com-moldura-de-madeira-e-lencois-brancos-isolados_176382-171.jpg',
+        authorCredits: 'Imagem de alexandercho no Freepik',
+        category: 'c',
+        seen: false,
+        id: 13
+    },
+    {
+        name: 'Casa',
+        image: 'https://img.freepik.com/fotos-gratis/vista-3d-do-modelo-de-casa_23-2150761168.jpg',
+        authorCredits: 'Imagem de Freepik',
+        category: 'c',
+        seen: false,
+        id: 14
+    },
+    {
+        name: 'Cepillo',
+        image: 'https://img.freepik.com/psd-gratuitas/icone-3d-de-beleza-e-maquiagem_23-2150582403.jpg',
+        authorCredits: 'Imagem de Freepik',
+        category: 'c',
+        seen: false,
+        id: 15
+    },
  
-]   
+]    
+
+
+
+
 export default allObjects;
